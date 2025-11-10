243bbf3696af722c4083a83cd1a6214128f6b2e4
Akulen <tomas@rigaux.com>
2025-11-10 16:38:26 +0900

    Added model helper functions

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,12 @@
 from models.linear_regression import LinearRegression
+
+def get_model(model_name: str):
+    match model_name:
+        case 'LinearRegression':
+            return LinearRegression
+        case _:
+            raise ValueError(f"Unknown model: {model_name}")
+
+def get_model_params(model_name: str):
+    import inspect
+    return inspect.signature(get_model(model_name).__init__).parameters
