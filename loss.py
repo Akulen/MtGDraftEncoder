14e61fdab4ee86635964160e7f57162e7877ece8
Akulen <tomas@rigaux.com>
2026-06-29 17:46:04 +0900

    Cleanup

--- loss.py
+++
@@ -1,8 +0,0 @@
-import torch.nn as nn
-
-class weighted_MSELoss(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, input, target, weight):
-        return ((input - target)**2 * weight).sum() / weight.sum()
